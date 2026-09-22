32cef7f1364903e381671adbe3bbc2781844c19a
Abdelrahman <apderahmanezat@gmail.com>
2026-09-22 18:18:22 +0300

    Fix Part 1 program

---
+++ Part1_ProceduralToOOP/src/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part1_ProceduralToOOP
+{
+
+    public class Customer
+    {
+
+        public int Id { get; }
+        public string Email { get; private set; }
+
+        public string City { get; set; }
+
+        public string Name { get; private set; }
+
+        public bool IsVip { get; private set; }
+
+        public Customer(int id, string email, string city, string name, bool isVip)
+        {
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException("id must be greater than zero");
+            if (string.IsNullOrWhiteSpace(email))
+                throw new NullReferenceException("email cannot be empt");
+            if (string.IsNullOrWhiteSpace(name))
+                throw new NullReferenceException("name cannot be empt");
+            if (string.IsNullOrWhiteSpace(city))
+                throw new NullReferenceException("city cannot be empty");
+            Id = id;
+            Email = email;
+            City = city;
+            Name = name;
+            IsVip = isVip;
+        }
+
+
+
+
+    }
+
+}
---
+++ Part1_ProceduralToOOP/src/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part1_ProceduralToOOP
+{
+    public class Product
+    {
+
+        public int Id { get; }
+        public string Name { get; private set; }
+
+        public double Price { get; private set; }
+
+        public int Stock { get; private set; }
+        public Product(int id, string name, double price, int stock)
+        {
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException("id must be greater than zero");
+            if (string.IsNullOrWhiteSpace(name))
+                throw new NullReferenceException("name cannot be empt");
+            if (price <= 0)
+                throw new ArgumentOutOfRangeException("price must be greater than zero");
+            if (stock <= 0)
+                throw new ArgumentOutOfRangeException("stock must be greater than zero");
+
+            Id = id;
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
+        public void ReduceStock(int quantity)
+        {
+            if (quantity > Stock)
+                throw new InvalidOperationException("Not enough stock.");
+
+            Stock -= quantity;
+        }
+
+
+    }
+}
