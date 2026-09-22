17d742b2bc3463f97c7737b45c73310cf1a52b18
Abdelrahman <apderahmanezat@gmail.com>
2026-09-22 18:27:50 +0300

    Implement hotel reservation system

--- Part2_HotelReservationSystem/src/Program.cs
+++ Part2_HotelReservationSystem/src/Program.cs
@@ -1,52 +1,10 @@
-namespace Part1_ProceduralToOOP;
-    public class Program
-     {
-    static void Main(string[] args)
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part2_HotelReservationSystem
+{
+    internal class Program
     {
-        // Customers
-        Customer customer1 = new Customer(
-            1,
-            "Cairo",
-            "apod@example.com",
-            "Apod",
-            true);
-
-        Customer customer2 = new Customer(
-            2,
-            "Alexandria",
-            "omar@example.com",
-            "Omar",
-            false);
-
-        // Products
-        Product pen = new Product(101, "Pen", 50, 100);
-        Product pad = new Product(102, "Pad", 100, 50);
-
-        // Create Order
-        Order order = new Order(customer1, DateOnly.FromDateTime(DateTime.Now));
-
-        // Add items
-        order.AddItem(pen, 2);
-        order.AddItem(pad, 1);
-
-        // Pay
-        order.Pay();
-
-        // Print
-        order.Print();
-
-        Console.WriteLine($"Pen Stock: {pen.Stock}");
-        Console.WriteLine($"Pad Stock: {pad.Stock}");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
